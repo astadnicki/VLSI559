05a2fa8cc8d2c72650a07f8d355bd875be51e6b5
dariusumass <166856425+dariusumass@users.noreply.github.com>
2024-04-15 17:29:02 -0400

    Update vending_tb.v

--- vending_tb.v
+++ vending_tb.v
@@ -5,8 +5,9 @@
 module vending_tb();
 
 reg clk;
+reg rst;
 reg paymentMethod;
-reg [32:0] index;
+reg [2:0] index;
 reg [63:0] cost;
 reg nickel, dime, quarter, dollar;
 reg cancel;
@@ -22,7 +23,7 @@ wire [2:0] pen;
 localparam CLK_PERIOD = 10;
 
 
-vending dut(clk, index, paymentMethod, creditBalance, nickel, dime, quarter, dollar, cost, cancel, dispensed, quart, dim, nick, pen);
+vending dut(clk, rst, index, paymentMethod, creditBalance, nickel, dime, quarter, dollar, cost, cancel, dispensed, quart, dim, nick, pen);
 
 initial clk = 0;
 always #CLK_PERIOD clk = ~clk;
@@ -30,7 +31,7 @@ always #CLK_PERIOD clk = ~clk;
 // declare inventory
 initial begin
 
-	// enter what inventory is available by index.
+	// enter what inventory is available by index
 	cost = {8'b10010110, 8'b1100100, 8'b11001000, 8'b1100100, 8'b1100100, 8'b1100100, 8'b1100100, 8'b1100100};	
 
 end
@@ -39,27 +40,55 @@ integer i;
 initial begin
 
 	cancel = 0;
-	paymentMethod = 0;
-	creditBalance = 8'b1100100;
+	paymentMethod = 1;
+	creditBalance = 8'b1100100 - 1;	//1 dollar
+	rst = 0;
 
 	// Select the product
 	index = 2;
-
-	// Insert coins
-	for (i=0; i<=3; i=i+1) begin	// insert 4 nickels
-		nickel=1;
-		dollar=1;
-		#15;
-		nickel=0;
-		dollar=0;
-		#15;
-	end
+	
+	
+	rst = 1;
+	#20;
+	rst = 0;
 	
 	#15;
+
+	// Insert coins
+	for (i=0; i<=3; i=i+1) begin	// insert 4 nickels and 4 dollars
+		nickel=1;
+		dollar=1;
+		$display("Money entered");
+		#10;
+		nickel=0;
+		dollar=0;
+		#10;
+	end
+	
+	#10;
 	cancel = 1;
 	$display("cancel");
-	#15;
+	#10;
 	cancel = 0;
+	
+	for (i=0; i<=3; i=i+1) begin	// insert 4 nickels and 4 dollars
+		nickel=1;
+		dollar=1;
+		$display("Money entered");
+		#10;
+		nickel=0;
+		dollar=0;
+		#10;
+	end
+	
+	index=0;
+	#10;
+	index = 1;
+	
+	cancel = 1;
+	#10;
+	cancel = 0;
+	
 	
 
 	
