d130eb43b43817d059aa54f9e254ea4498339783
dariusumass <166856425+dariusumass@users.noreply.github.com>
2024-04-15 17:29:29 -0400

    Update dispenseChange.v

--- dispenseChange.v
+++ dispenseChange.v
@@ -5,12 +5,16 @@ output reg [3:0] quarters;
 output reg [2:0] dimes;
 output reg [2:0] nickels;
 output reg [2:0] pennies;
+
+localparam QUARTER = 25;
+localparam DIME = 10;
+localparam NICKEL = 5;
 	
-always @* begin
-	quarters = change / 25;
-	dimes = (change % 25) / 10;
-	nickels = ((change % 25) % 10) / 5;
-	pennies = (((change % 25) % 10) % 5);
+always @(change) begin
+	quarters = change / QUARTER;
+	dimes = (change % QUARTER) / DIME;
+	nickels = ((change % QUARTER) % DIME) / NICKEL;
+	pennies = (((change % QUARTER) % DIME) % NICKEL);
 end
 	
 
